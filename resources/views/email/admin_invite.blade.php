89546126167219a45838bf6dba3a31f9f7d31260
Benjamin Isong <beisongabc@gmail.com>
2020-09-02 13:04:53 +0100

    email ui update

--- resources/views/email/admin_invite.blade.php
+++ resources/views/email/admin_invite.blade.php
@@ -89,7 +89,7 @@
                     <tbody>
                     <tr>
                         <td style="text-align: center; padding-bottom:25px">
-                            <a href="#"><img style="height: 40px" src="{{ url('images/logo-black.png') }}" alt="logo"></a>
+                            <a href="#"><img style="height: 40px" src="{{ url('images/logo-black.png') }}" alt=""></a>
                             <p style="font-size: 14px; color: #A70C17; padding-top: 12px;">Winners Durumi | Admin Invite</p>
                         </td>
                     </tr>
@@ -107,15 +107,15 @@
                             <p style="margin-bottom: 10px;">Hi {{ $name }},</p>
                             <p style="margin-bottom: 25px;">You have been invited to be and admin on the Winners Durumi Web Portal. </p>
                             <p style="margin-bottom: 10px;">If you wish to accept this invite, click the button below.</p>
-                            <a href="{{ $link }}" style="background-color:#A70C17;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 25px">Reset Password</a>
+                            <a href="{{ $link }}" style="background-color:#A70C17;border-radius:4px;color:#ffffff;display:inline-block;font-size:13px;font-weight:600;line-height:44px;text-align:center;text-decoration:none;text-transform: uppercase; padding: 0 25px">Accept Invite</a>
                             <p style="margin-bottom: 25px;">or use this link on a web browser.</p>
-                            <p style="margin-bottom: 10px;"><a href="{{ $link }}">{{ $link }}</a></p>
+                            <p style="margin-bottom: 25px;"><a href="{{ $link }}" style="color: #A70C17">{{ $link }}</a></p>
                         </td>
                     </tr>
                     <tr>
                         <td style="text-align:center;padding: 20px 30px 40px">
                             <p>If you did not make accept this request, please ignore this email. The invite link expires after 56 hours if ignored.</p>
-                            <p style="margin: 0; font-size: 13px; line-height: 22px; color:rgba(191,0,1,0.55);">This is an automatically generated email please do not reply to this email. If you face any issues, please contact us at {{ env('SUP', '') }}</p>
+                            <p style="margin: 0; font-size: 13px; line-height: 22px; color:rgba(191,0,1,0.55);">This is an automatically generated email please do not reply to this email. If you face any issues, please contact us at {{ env('SUP', 'dev@winnersdurumi.org') }}</p>
                         </td>
                     </tr>
                     </tbody>
@@ -126,10 +126,10 @@
                         <td style="text-align: center; padding:25px 20px 0;">
                             <p style="font-size: 13px;">Copyright © {{ date('Y') }} {{ env('APP_NAME', '') }}. All rights reserved. <br> supported by Technical Unit & <a style="color: #A70C17; text-decoration:none;" href="https://synergynode.com">Synergy Node</a>.</p>
                             <ul style="margin: 10px -4px 0;padding: 0;">
-                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-b.png') }}" alt="brand"></a></li>
-                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-e.png') }}" alt="brand"></a></li>
-                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-d.png') }}" alt="brand"></a></li>
-                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-c.png') }}" alt="brand"></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-b.png') }}" alt=""></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-e.png') }}" alt=""></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-d.png') }}" alt=""></a></li>
+                                <li style="display: inline-block; list-style: none; padding: 4px;"><a style="display: inline-block; height: 30px; width:30px;border-radius: 50%; background-color: #ffffff" href="#"><img style="width: 30px" src="{{ url('images/brand-c.png') }}" alt=""></a></li>
                             </ul>
                             <p style="padding-top: 15px; font-size: 12px;">This email was sent to you as an invited user of <a style="color: #A70C17; text-decoration:none;" href="https://softnio.com">{{ env('APP_NAME', '') }}</a>. To update your emails preferences <a style="color: #a70c17; text-decoration:none;" href="{{ env('APP_URL', '') }}">{{ env('APP_NAME', '') }}</a>.</p>
                         </td>
